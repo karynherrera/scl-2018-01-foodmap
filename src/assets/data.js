16db5757ce07c0a8779c7376c65ed504002410db
Karyn Herrera <karynherrera@gmail.com>
2018-07-25 11:41:37 -0400

    añadiendo api de google

---
+++ src/assets/data.js
@@ -0,0 +1,60 @@
+window.restaurants = [];
+window.map;
+window.infowindow;
+
+const usingApi = (() => {
+  // Creamos un mapa con las coordenadas actuales
+  navigator.geolocation.getCurrentPosition((pos) => {
+    lat = pos.coords.latitude;
+    lon = pos.coords.longitude;
+
+    let myLatlng = new google.maps.LatLng(lat, lon);
+
+    let mapOptions = {
+      center: myLatlng,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.MAP
+    };
+
+    map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+
+    // Creamos el infowindow
+    infowindow = new google.maps.InfoWindow();
+
+    // Especificamos la localización, el radio y el tipo de lugares que queremos obtener
+    let request = {
+      location: myLatlng,
+      radius: 5000,
+      types: ['restaurant']
+    };
+
+    // Creamos el servicio PlaceService y enviamos la petición.
+    let service = new google.maps.places.PlacesService(map);
+
+    service.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        restaurants = results;
+        console.log(restaurants);
+        for (var i = 0; i < results.length; i++) {
+          crearMarcador(results[i]);
+          console.log(results[i].name);
+        }
+      }
+    });
+  });
+})
+
+
+const crearMarcador = ((place) => {
+  // Creamos un marcador
+  let marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  // Asignamos el evento click del marcador
+  google.maps.event.addListener(marker, 'click', () => {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}); 
